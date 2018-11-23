6a07e872bef56fca95e4b0d8e17c86e1b290ccf7
Radhwane Chebaane <radcheb@gmail.com>
2018-11-24 00:21:53 +0100

    dash example

--- data_viz/app.py
+++ data_viz/app.py
@@ -4,21 +4,12 @@
 
 import os
 import pandas as pd
-import datetime
-from itertools import product
+import dateutil
 
-from bokeh.core.properties import value
-from bokeh.events import Reset
-from bokeh.events import SelectionGeometry
-from bokeh.layouts import column
-from bokeh.models import ColumnDataSource
-from bokeh.models import HoverTool
-from bokeh.models import NumeralTickFormatter
-from bokeh.models.widgets import CheckboxButtonGroup
-from bokeh.models.widgets import Div
-from bokeh.plotting import figure
-from bokeh.plotting import curdoc
-
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
 
 #####   Data loading ####
 
@@ -36,154 +27,123 @@ STATES = ['successful', 'suspended', 'failed', 'canceled']
 
 print(kickstarter_df.describe())
 
-#####   Interactions ####
+#####   Main  ####
 
-def filter_categories(indexes):
-    categories_filtered = [CATEGORIES[ind] for ind in indexes] if indexes != [] else CATEGORIES
-    update_num_categories_source(categories_filtered, force_categories=True)
-    update_usd_vs_date_source(categories_filtered, force_categories=True)
+app = dash.Dash()
 
-
-def update_usd_vs_date_source(categories=None, force_categories=False):
-    # To get the current state of the categories
-    if categories is None:
-        categories = source_num_categories.data['categories']
-    elif not force_categories:
-        categories = list(set(categories) & set(source_num_categories.data['categories']))
-    df_categories = kickstarter_df_sub[kickstarter_df_sub.broader_category.isin(categories)]
-    for color, state in zip(COLORS, STATES):
-        df_by_state = df_categories[df_categories.state == state]
-        data = {
-            'x': df_by_state['created_at'],
-            'y': df_by_state['usd_pledged'],
-            'name': df_by_state['name'],
-            'state': [state] * len(df_by_state),
-        }
-        sources_usd_vs_date[state].data = data
-
-
-def update_on_usd_vs_date_selection(event):
-    geometry = event.geometry
-    if geometry['type'] == 'rect':
-        x0 = datetime.datetime.fromtimestamp(geometry['x0'] / 1000)
-        x1 = datetime.datetime.fromtimestamp(geometry['x1'] / 1000)
-        y0 = geometry['y0']
-        y1 = geometry['y1']
-        sub_df = kickstarter_df[kickstarter_df.created_at.between(x0, x1) & kickstarter_df.usd_pledged.between(y0, y1)]
-        update_num_categories_source(source_num_categories.data['categories'], sub_df)
-
-
-def update_num_categories_source(categories=None, df=kickstarter_df, force_categories=False):
-    # To get the current state of the categories
-    if categories is None:
-        categories = source_num_categories.data['categories']
-    elif not force_categories:
-        categories = list(set(categories) & set(source_num_categories.data['categories']))
-    stacked_barchart_df = (
-        df['state'].groupby(df['broader_category'])
-            .value_counts(normalize=False)
-            .rename('count')
-    )
-
-    data = {
-        'categories': categories,
-    }
-
-    # Sadly, I could not find a more efficient method to prepare a pandas array for a stacked bar chart
-    for state, category in product(STATES, categories):
-        data.setdefault(state, [])
-
-        try:
-            data[state].append(stacked_barchart_df.loc[category, state])
-        except KeyError:
-            data[state].append(0)
-    source_num_categories.data = data
-
-
-#####   Data sources ####
-
-sources_usd_vs_date = {state: ColumnDataSource({
-    'x': [],
-    'y': [],
-    'name': [],
-    'state': [],
-}) for state in STATES}
-
-source_num_categories = ColumnDataSource(data={
-    'categories': CATEGORIES
+# This is optional but the app looks better with it
+app.css.append_css({
+    "external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"
 })
-
-update_usd_vs_date_source()
-update_num_categories_source()
-
 
 #####   Layout   ####
 
-title = Div(text='<h1 style="text-align: center">Kickstarter Dashboard</h1>')
-
-# This looks better than the multiselect widget
-select = CheckboxButtonGroup(labels=CATEGORIES)
-select.on_click(filter_categories)
-
-hover_usd_vs_date = HoverTool(tooltips=[
-    ("Name", "@name"),
-    ("State", "@state"),
-])
-
-p_usd_vs_date = figure(
-    plot_height=200,
-    y_axis_type='log',
-    x_axis_type='datetime',
-    tools=[hover_usd_vs_date, 'box_select', 'reset']
-)
-
-for color, state in zip(COLORS, STATES):
-    p_usd_vs_date.circle(
-        x='x',
-        y='y',
-        line_color='white',
-        fill_color=color,
-        alpha=0.7,
-        size=15,
-        legend=state,
-        source=sources_usd_vs_date[state]
+app.layout = html.Div(children=[
+    html.H1(children='Kickstarter Dashboard', style={
+        'textAlign': 'center',
+    }),
+    dcc.Dropdown(
+        id='categories',
+        options=[{'label': i, 'value': i} for i in kickstarter_df['broader_category'].unique()],
+        multi=True
+    ),
+    dcc.Graph(
+        id='usd-pledged-vs-date',
+    ),
+    dcc.Graph(
+        id='count-state-vs-category',
     )
-
-p_usd_vs_date.xaxis.axis_label = 'Date'
-p_usd_vs_date.yaxis.axis_label = 'USD pledged'
-# See http://bokeh.pydata.org/en/latest/docs/reference/models/formatters.html for all formatters
-p_usd_vs_date.yaxis.formatter = NumeralTickFormatter(format='0a')
-p_usd_vs_date.legend.click_policy = 'hide'
-p_usd_vs_date.legend.location = "top_left"
-
-p_usd_vs_date.on_event(Reset, lambda _: update_num_categories_source())
-p_usd_vs_date.on_event(SelectionGeometry, update_on_usd_vs_date_selection)
-
-# Can't seem to be able to put the state in there or the number of student in the tooltip though
-hover_num_categories = HoverTool(tooltips=[
-    ("Category", "@categories"),
 ])
 
-p_num_categories = figure(
-    x_range=CATEGORIES,
-    plot_height=200,
-    tools=[hover_num_categories]
-)
 
-p_num_categories.vbar_stack(
-    STATES,
-    x='categories',
-    width=0.9,
-    color=COLORS,
-    source=source_num_categories,
-    legend=[value(x) for x in STATES]
-)
+#####   Interactions ####
 
-p_num_categories.yaxis.axis_label = 'Number of projects'
+@app.callback(
+    dash.dependencies.Output('usd-pledged-vs-date', 'figure'),
+    [
+        dash.dependencies.Input('categories', 'value'),
+    ])
+def update_scatterplot(categories):
+    if categories is None or categories == []:
+        categories = CATEGORIES
 
-layout = column(title, select, p_usd_vs_date, p_num_categories, sizing_mode='scale_width')
+    sub_df = kickstarter_df_sub[(kickstarter_df_sub['broader_category'].isin(categories))]
+
+    return {
+        'data': [
+            go.Scatter(
+                x=sub_df[(kickstarter_df_sub.state == state)]['created_at'],
+                y=sub_df[(kickstarter_df_sub.state == state)]['usd_pledged'],
+                text=sub_df[(kickstarter_df_sub.state == state)]['name'],
+                mode='markers',
+                opacity=0.7,
+                marker={
+                    'size': 15,
+                    'color': color,
+                    'line': {'width': 0.5, 'color': 'white'}
+                },
+                name=state,
+            ) for (state, color) in zip(STATES, COLORS)
+        ],
+        'layout': go.Layout(
+            xaxis={'title': 'Date'},
+            yaxis={'title': 'USD pledged', 'type': 'log'},
+            margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+            legend={'x': 0, 'y': 1},
+            hovermode='closest'
+        )
+    }
 
 
-#####   Main  ####
+@app.callback(
+    dash.dependencies.Output('count-state-vs-category', 'figure'),
+    [
+        dash.dependencies.Input('categories', 'value'),
+        dash.dependencies.Input('usd-pledged-vs-date', 'relayoutData')
+    ])
+def update_bar_chart(categories, relayout_data):
+    if categories is None or categories == []:
+        categories = CATEGORIES
 
-curdoc().add_root(layout)
+    if relayout_data is not None and ('xaxis.autorange' in relayout_data) and ('yaxis.autorange' in relayout_data):
+        print(relayout_data)
+        x0 = dateutil.parser.parse(relayout_data['xaxis.range[0]'])
+        x1 = dateutil.parser.parse(relayout_data['xaxis.range[1]'])
+        y0 = 10 ** relayout_data['yaxis.range[0]']
+        y1 = 10 ** relayout_data['yaxis.range[1]']
+
+        sub_df = kickstarter_df[kickstarter_df.created_at.between(x0, x1) & kickstarter_df.usd_pledged.between(y0, y1)]
+    else:
+        sub_df = kickstarter_df
+
+    stacked_barchart_df = (
+        sub_df[sub_df['broader_category'].isin(categories)]['state'].groupby(sub_df['broader_category'])
+            .value_counts(normalize=False)
+            .rename('count')
+            .to_frame()
+            .reset_index('state')
+            .pivot(columns='state')
+            .reset_index()
+    )
+    return {
+        'data': [
+            go.Bar(
+                x=stacked_barchart_df['broader_category'],
+                y=stacked_barchart_df['count'][state],
+                name=state,
+                marker={
+                    'color': color
+                }
+            ) for (state, color) in zip(STATES[::-1], COLORS[::-1])
+        ],
+        'layout': go.Layout(
+            yaxis={'title': 'Number of projects'},
+            barmode='stack',
+            hovermode='closest'
+        )
+    }
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run_server(debug=True, host='0.0.0.0', port=port)
